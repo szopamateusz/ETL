d7ee97b6d8594173b2efe1694118744d35f9d210
Mateusz Szopa <github@mateusz-szopa.pl>
2019-12-07 12:03:44 +0100

    Add basic models

---
+++ server/ETL.API/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ETL.API.Models
+{
+    public class Review
+    {
+        public DateTime ReviewDate { get; set; }
+        public string ReviewerName { get; set; }
+        public string ProductRating { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewText { get; set; }
+        public string ReviewRating { get; set; }
+        public List<ReviewComment> ReviewComments { get; set; }
+    }
+}
---
+++ server/ETL.API/Models/ReviewComment.cs
@@ -0,0 +1,9 @@
+﻿namespace ETL.API.Models
+{
+    public class ReviewComment
+    {
+        public string UserName { get; set; }
+        public string ReviewTime { get; set; }
+        public string Comment { get; set; }
+    }
+}
