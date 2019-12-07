0ac7283e4504e2411bd3506b4bffc36be4ecb060
Mateusz Szopa <github@mateusz-szopa.pl>
2019-12-07 20:17:48 +0100

    Update models

--- server/ETL.API/Models/Review.cs
+++ server/ETL.API/Models/Review.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ETL.API.Models
+﻿namespace ETL.API.Models
 {
     public class Review
     {
-        public DateTime ReviewDate { get; set; }
+        public string ReviewDate { get; set; }
         public string ReviewerName { get; set; }
         public string ProductRating { get; set; }
         public string ReviewTitle { get; set; }
         public string ReviewText { get; set; }
-        public string ReviewRating { get; set; }
-        public List<ReviewComment> ReviewComments { get; set; }
     }
 }
--- server/ETL.API/Models/ReviewComment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ETL.API.Models
-{
-    public class ReviewComment
-    {
-        public string UserName { get; set; }
-        public string ReviewTime { get; set; }
-        public string Comment { get; set; }
-    }
-}
