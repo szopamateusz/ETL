0819f10977dc18201595e5586f696a1eb31b79b9
Mateusz Szopa <github@mateusz-szopa.pl>
2019-12-07 12:04:03 +0100

    Create basic controller endpoints

---
+++ server/ETL.API/Controllers/EtlController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETL.API.Controllers
+{
+    [Route("/etl/api")]
+    [ApiController]
+    public class EtlController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetReviews()
+        {
+            return Ok();
+        }
+
+        [HttpPost("extract")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Extract([FromBody] string url)
+        {
+            return Ok();
+        }
+
+        [HttpPost("transform")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Transform()
+        {
+            return Ok();
+        }
+
+        [HttpPost("load")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Load()
+        {
+            return Ok();
+        }
+
+        [HttpPost("etl")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> ETL([FromBody] string url)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("clear")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Clear()
+        {
+            return Ok();
+        }
+    }
+}
